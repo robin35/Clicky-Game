093a09ccfc547a5b53c59d155cd97bc91c7257fd
Terra Robinson <tlrobinson179@gmail.com>
2019-10-08 21:38:56 -0500

    Updates

---
+++ precache-manifest.a622437baa83558f09fdda9f245ee725.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f85f4e99c5aab7fd919f889ee77f6ec",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b64b903c8b4c2163d8d7",
+    "url": "/clicky-game/static/css/main.6348c129.chunk.css"
+  },
+  {
+    "revision": "8b28b04b5b06edd2dc22",
+    "url": "/clicky-game/static/js/2.dc948b19.chunk.js"
+  },
+  {
+    "revision": "b64b903c8b4c2163d8d7",
+    "url": "/clicky-game/static/js/main.6f4104ae.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
